ff11af840b1955ad207ab332b6b1f26943956a89
Raj Govind Jangid <rajgovindjangid11@gmail.com>
2022-10-16 20:41:15 +0530

    user login validation added

--- src/validations/user/user.js
+++ src/validations/user/user.js
@@ -10,5 +10,7 @@ exports.registerValidation = [
     check('fullname').exists().withMessage("Fullname field is required"),
     check('email').exists().withMessage("Name field is required")
     .isEmail().withMessage("Email id is invalid"),
-    check('password').exists().withMessage("Price field is required"),
+    check('password').exists().withMessage("Password field is required"),
+    check('confirmPassword').exists().withMessage("Confirm Password field is required")
+    .matches('password').withMessage('Passwords must match.')
 ]
